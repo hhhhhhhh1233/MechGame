8923e0092b7f9dbcf480b4c92c5e0df19f9733f0
Tage <tagetjerngren@gmail.com>
2023-06-20 02:48:12 +0200

    Added more dummies, messed with lighting and added keyboard controls

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 10.0
 const JUMP_VELOCITY = 6
 const JUMP_ACCEL = 0.1
+const LOOK_SPEED = 3
+const LOOK_ANGLE = 0.8
 
 @onready var BULLET = preload("res://bullet.tscn")
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -24,29 +26,29 @@ func _physics_process(delta):
 		var bullet =  BULLET.instantiate()
 		$"..".add_child(bullet)
 		bullet.transform = transform
-		bullet.position += bullet.global_transform.basis.z * -2
+		bullet.position += bullet.global_transform.basis.z * -1
 
 	if Input.is_action_just_pressed("pause"):
 		get_tree().quit()
 
 	# Handle Jump.
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("fly"):
 		velocity.y += JUMP_VELOCITY * JUMP_ACCEL
 		if velocity.y > JUMP_VELOCITY:
 			velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("walk_left", "walk_right", "walk_forward", "walk_back")
 	var look_dir = Input.get_vector("look_left","look_right", "look_up", "look_down")
 	
 	if look_dir:
-		rotation.x += -look_dir.y * delta * 3
-		if rotation.x > 0.8:
-			rotation.x = 0.8
-		if rotation.x < -0.8:
-			rotation.x = -0.8
-		rotation.y += -look_dir.x * delta * 3
+		rotation.x += -look_dir.y * delta * LOOK_SPEED
+		if rotation.x > LOOK_ANGLE:
+			rotation.x = LOOK_ANGLE
+		if rotation.x < -LOOK_ANGLE:
+			rotation.x = -LOOK_ANGLE
+		rotation.y += -look_dir.x * delta * LOOK_SPEED
 	
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
